d247c253bc7068673dd3c4dacb9bb6d4ead32233
502y <53784463+502y@users.noreply.github.com>
2024-04-06 13:55:39 +0800

    Add Navigator

--- lib/blog_name.dart
+++ lib/blog_name.dart
@@ -1,7 +1,16 @@
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+
 const List<String> flutterName = ["ListView", "HTML初步"];
 const List<String> blogType = [
   "Flutter",
   "HTML",
   "CSS(building)",
   "JS(Building)"
+];
+
+const List<NavigationRailDestination> destination = [
+  NavigationRailDestination(icon: FlutterLogo(), label: Text("Flutter")),
+  NavigationRailDestination(icon: Icon(Icons.html), label: Text("HTML")),
 ];
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 import '../blog_name.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
   static const String router = '/home';
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final PageController pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
+    //构建点击按钮
     Widget clickableTextMenu(int i) {
       return GestureDetector(
         onTap: () {
@@ -20,14 +29,45 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         automaticallyImplyLeading: false,
       ),
-      body: Container(
-        alignment: Alignment.topCenter,
-        child: ListView.builder(
-            controller: ScrollController(),
-            itemCount: blogType.length,
-            itemBuilder: (BuildContext context, int i) {
-              return clickableTextMenu(i);
-            }),
+      body: Row(
+        children: [
+          NavigationRail(
+            trailing: const Expanded(
+                child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: FlutterLogo(),
+              ),
+            )),
+            onDestinationSelected: (i) => pageController.jumpToPage(i),
+            destinations: destination,
+            selectedIndex: 1,
+          ),
+          Expanded(
+            child: PageView(controller: pageController, children: [
+              //Flutter部分
+              Container(
+                alignment: Alignment.topLeft,
+                child: ListView.builder(
+                    controller: ScrollController(),
+                    itemCount: flutterName.length,
+                    itemBuilder: (BuildContext context, int i) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            flutterName[i],
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+              Text("2")
+            ]),
+          ),
+        ],
       ),
     );
   }
